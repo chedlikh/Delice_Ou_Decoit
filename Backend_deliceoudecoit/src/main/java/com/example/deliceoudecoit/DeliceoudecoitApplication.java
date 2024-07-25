6794ee8d2a5704bf5e3c43416e93ff395ebc653d
chedlikh <khangui.mohamedchedli@esprit.tn>
2024-07-25 16:16:35 +0100

    Add FrontEnd_DeliceOuDecoit folder with proper line endings

---
+++ Backend_deliceoudecoit/src/main/java/com/example/deliceoudecoit/DeliceoudecoitApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliceoudecoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliceoudecoitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliceoudecoitApplication.class, args);
+    }
+
+
+}
---
+++ Backend_deliceoudecoit/src/main/java/com/example/deliceoudecoit/dao/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.deliceoudecoit.dao;
+
+import com.example.deliceoudecoit.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
