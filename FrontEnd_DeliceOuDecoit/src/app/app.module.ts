46b6ee117e1ad91d3a73d9b81885e61f5e67f004
chedlikh <khangui.mohamedchedli@esprit.tn>
2024-08-16 19:28:45 +0100

    homePage

--- FrontEnd_DeliceOuDecoit/src/app/app.module.ts
+++ FrontEnd_DeliceOuDecoit/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from 'src/navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
